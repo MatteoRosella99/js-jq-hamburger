43aea44cf4d45cca99eda29473d71a30449f6568
Matteo <rosella.matteo@gmail.com>
2020-03-31 16:40:25 +0200

    tempistiche

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 $('.header-right > a').click(
   function(){
     $('.hamburger-menu').fadeIn(1000);
-    $('.header-right > a').fadeOut(1000);
+    $('.header-right > a').fadeOut(500);
     $('.close').fadeIn(1000);
   }
 )
@@ -10,6 +10,6 @@ $('.close').click(
   function(){
     $('.hamburger-menu').fadeOut(1000);
     $('.close').fadeOut(1000);
-    $('.header-right > a').fadeIn(1000);
+    $('.header-right > a').fadeIn(500);
   }
 )
