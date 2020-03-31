3e7c174afe492df4afed5dfc8b341a20151e9c76
Matteo <rosella.matteo@gmail.com>
2020-03-31 16:38:23 +0200

    Programma menu a tendina

---
+++ main.js
@@ -0,0 +1,15 @@
+
+$('.header-right > a').click(
+  function(){
+    $('.hamburger-menu').fadeIn(1000);
+    $('.header-right > a').fadeOut(1000);
+    $('.close').fadeIn(1000);
+  }
+)
+$('.close').click(
+  function(){
+    $('.hamburger-menu').fadeOut(1000);
+    $('.close').fadeOut(1000);
+    $('.header-right > a').fadeIn(1000);
+  }
+)
